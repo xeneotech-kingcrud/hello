c10e9ad8cdd6c25d00c66e50e7a474bf6c754581
roxanmlr <roxanmlr@gmail.com>
2024-11-06 11:49:01 +0100

    Added function F in sample

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ package main
 import (
 	"fmt"
 	"github.com/xeneotech-kingcrud/greetings"
+	"github.com/xeneotech-kingcrud/hello/sample"
  )
 
 func main(){
 	message := greetings.Hello("Gladys")
+	F()
 	fmt.Println(message)
 }
  
